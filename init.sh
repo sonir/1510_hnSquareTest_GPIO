9403e43e2048e2c2a460184786b49595679142a7
sonir <isana@sonir.jp>
2015-09-16 19:02:10 +0900

    2nd

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# 1510_hnSquareTest_GPIO" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sonir/1510_hnSquareTest_GPIO.git
+git push -u origin master
+
---
+++ square.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+
+COUNT = 3
+PIN = 11
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN,GPIO.OUT)
+
+# for _ in xrange(COUNT):
+#     GPIO.output(PIN,True)
+#    time.sleep(1.0)
+#     GPIO.output(PIN,False)
+#     time.sleep(1.0)
+
+while True :
+	GPIO.output(PIN,True)
+	time.sleep(1.0)
+	GPIO.output(PIN,False)
+	time.sleep(1.0)
+
+
+
+GPIO.cleanup()
+
+
