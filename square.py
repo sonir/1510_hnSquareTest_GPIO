fbfc05715c0cc93d6a77d96f00c8dac918081b6a
sonir <isana@sonir.jp>
2015-09-29 23:07:30 +0900

    merged 2.1

--- square.py
+++ square.py
@@ -7,26 +7,6 @@ PIN = 11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN,GPIO.OUT)
 
-# for _ in xrange(COUNT):
-#     GPIO.output(PIN,True)
-#    time.sleep(1.0)
-#     GPIO.output(PIN,False)
-#     time.sleep(1.0)
-
-<<<<<<< HEAD
-while True :
-	GPIO.output(PIN,True)
-	time.sleep(1.0)
-	GPIO.output(PIN,False)
-	time.sleep(1.0)
-
-#except KeyboardInterrupt:
-# print("detect key interrupt\n")
- 
-#GPIO.cleanup()
-#print("Program exit\n")
-
-=======
 try:
 	while True :
 		GPIO.output(PIN,True)
@@ -36,4 +16,4 @@ try:
 
 except KeyboardInterrupt :
 	GPIO.cleanup()
->>>>>>> f480d7a9b984f648c18f5d013f8fe5b06c2a60c5
+
