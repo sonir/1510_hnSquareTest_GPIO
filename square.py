b44321bd3da9b8eb800202f120ac11c135a992ed
sonir <isana@sonir.jp>
2015-09-30 05:35:35 +0900

    v3.2b stable click is ok

--- square.py
+++ square.py
@@ -2,7 +2,7 @@
 import RPi.GPIO as GPIO
 import time, sq_osc
 
-sqo = sq_osc.SquareOsc(0.5)
+sqo = sq_osc.SquareOsc(0.002)
 sqo.bang()
 COUNT = 3
 PIN = 11
@@ -14,6 +14,7 @@ try:
 		flg = sqo.update()
 		if flg == 1 :
 			GPIO.output(PIN,True)
+			time.sleep(0.1)
 			print("ON")
 		elif  flg == 0 :
 			GPIO.output(PIN,False)
