3dba7830e849e886c519ac5420fa548d7a1f74ed
sonir <isana@sonir.jp>
2015-09-30 20:30:03 +0900

    v3.3 test sl_GPIO

---
+++ setup_gpio.py
@@ -0,0 +1,8 @@
+PIN_A = 3
+PIN_B = 5
+PIN_C = 7
+PIN_D = 11
+PIN_E = 13
+PIN_F = 15
+
+pins = [PIN_A, PIN_B, PIN_C, PIN_D, PIN_E, PIN_F]
---
+++ sl_gpio.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import setup_gpio
+
+class sl_gpio :
+
+try:
+    while True :
+        def __init__ (self) :
+            print("init")
+            global pins
+            self.GPIO.setmode(GPIO.BOARD)
+            for self.pin in pins
+                print self.pin
+                self.GPIO.setup(self.pi,GPIO.OUT)
+
+        def set (self, pin, val) :
+            print("set")
+
+        def update() :
+            pass
+
+except KeyboardInterrupt :
+	GPIO.cleanup()
