c9ff1ed0fcaceea0d65a64e8cc94d45f3477fe67
sonir <isana@sonir.jp>
2015-09-30 20:33:31 +0900

    mod

--- sl_gpio.py
+++ sl_gpio.py
@@ -3,21 +3,21 @@ import setup_gpio
 
 class sl_gpio :
 
-try:
-    while True :
-        def __init__ (self) :
-            print("init")
-            global pins
-            self.GPIO.setmode(GPIO.BOARD)
-            for self.pin in pins
-                print self.pin
-                self.GPIO.setup(self.pi,GPIO.OUT)
+    try:
+        while True :
+            def __init__ (self) :
+                print("init")
+                global pins
+                self.GPIO.setmode(GPIO.BOARD)
+                for self.pin in pins
+                    print self.pin
+                    self.GPIO.setup(self.pi,GPIO.OUT)
 
-        def set (self, pin, val) :
-            print("set")
+            def set (self, pin, val) :
+                print("set")
 
-        def update() :
-            pass
+            def update() :
+                pass
 
-except KeyboardInterrupt :
-	GPIO.cleanup()
+    except KeyboardInterrupt :
+    	GPIO.cleanup()
