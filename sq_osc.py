993f228e8dbff9eadf8550da5c42d01744a9a3dd
sonir <isana@sonir.jp>
2015-10-03 18:01:24 +0900

    merge

--- sq_osc.py
+++ sq_osc.py
@@ -1,7 +1,8 @@
 import sl_metro
 
 class SquareOsc:
-    
+
+    def __init__(self, interval):
         self.metro = sl_metro.Metro(interval)
         self.blinker = False
         self.counter = 0
@@ -12,27 +13,27 @@ class SquareOsc:
         self.click_times = 4
         self.bang_flg = False
 
-    def bang(self):
-        self.bang_flg = True
+        def bang(self) :
+            self.bang_flg = True
 
     def update (self) :
-            if self.metro.update() :
-                if(self.rotation < self.click_times):
-                    if self.now >= self.list_max :
-                        self.now = 0
-                        self.rotation += 1
-                        return 0
-                    elif self.list[self.now]==1:
-                        self.now +=1
-                        return 1
-                    else:
-                        self.now +=1
-                        # print("off")
-                        return 0
+        if self.metro.update() :
+            if(self.rotation < self.click_times):
+                if self.now >= self.list_max :
+                    self.now = 0
+                    self.rotation += 1
+                    return 0
+                elif self.list[self.now]==1:
+                    self.now +=1
+                    return 1
                 else:
-                    return -1
+                    self.now +=1
+                    # print("off")
+                    return 0
             else:
                 return -1
+        else:
+            return -1
                     # print("rest")
 
 
